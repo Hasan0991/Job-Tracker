a3c266e0a0f8885f97497b30ad58992c694b1880
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-17 15:24:42 +0200

    added user router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
-from .database import engine
+from app.routers import users  #
 from app import models
-from app.database import Base
+from app.database import engine
+
 
 models.Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI()
+
+
+app.include_router(users.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello fiudshfuiash"}
+    return {"message": "Job Tracker backend is running"}
