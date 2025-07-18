a37573fdc33ec13ca6d3e67ead8e97a8c14bf31d
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-18 17:35:43 +0200

    def to class and added none

--- backend/app/schemas/schemas.py
+++ backend/app/schemas/schemas.py
@@ -20,11 +20,10 @@ class UserResponse(BaseModel):
     updated_at: Optional[datetime]
 
     class Config:
-        orm_mode = True
-
-def UserUpdate(BaseModel):
-    email: Optional[EmailStr]
-    password: Optional[str]
-    first_name: Optional[str]
-    last_name: Optional[str]
-    birth_date: Optional[str]
+        from_attributes = True
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    birth_date: Optional[date] = None
