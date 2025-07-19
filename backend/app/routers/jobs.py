da692ad8ef10c5bd3e692ce648a7dfc33c530a62
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-19 21:04:40 +0200

    all jobs endpoint

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -14,3 +14,14 @@ def create_job(job:schemas.JobCreate,db:Session=Depends(get_db)):
     if not db_job:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No such user")
     return crud.create_job(db=db,job=job)
+
+@router.get("/{job_id}",response_model=schemas.JobResponse)
+def get_job_by_id(job_id:int ,db:Session=Depends(get_db)):
+    job=crud.get_jobs(db=db,job_id=job_id)
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Job not found")
+    return job
+
+@router.get("/",response_model=list[schemas.JobResponse],status_code=status.HTTP_201_CREATED)
+def get_all_jobs(db:Session=Depends(get_db)):
+    return crud.get_all_jobs(db)
