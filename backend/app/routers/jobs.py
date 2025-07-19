031c5650b330dc3a6cd5210de2202ee929837af9
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-19 13:56:10 +0200

    check for the existence of user

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -3,11 +3,14 @@ from sqlalchemy.orm import Session
 from app import crud,models
 from app.database import get_db
 from app.schemas import schemas
-
+from app.models import User
 router=APIRouter(
     prefix="/jobs",
     tags=["Jobs"]
 )
-@router.post("/",responce_model=schemas.JobResponse,status_code=status.HTTP_201_CREATED)
+@router.post("/",response_model=schemas.JobResponse,status_code=status.HTTP_201_CREATED)
 def create_job(job:schemas.JobCreate,db:Session=Depends(get_db)):
+    db_job= db.query(User).filter(User.id==job.user_id).first()
+    if not db_job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No such user")
     return crud.create_job(db=db,job=job)
