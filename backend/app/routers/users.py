7fe113eb204133e67b76e73a98aa2982fdfde6d2
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-19 21:26:06 +0200

    get all users function

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -9,9 +9,18 @@ router = APIRouter(
     prefix="/users",
     tags=["Users"]
 )
+
+
 @router.post("/", response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
+
+
+
+@router.get("/",response_model=list[schemas.UserResponse],status_code=status.HTTP_200_OK)
+def get_all_users(db:Session=Depends(get_db)):
+    return db.query(models.User).all()
+
 
 @router.get("/{user_id}",response_model=schemas.UserResponse)
 def get_user(user_id:int ,db:Session=Depends(get_db)):
@@ -20,9 +29,12 @@ def get_user(user_id:int ,db:Session=Depends(get_db)):
         raise HTTPException(status_code=404,detail="User not found")
     return user
 
+
+
 @router.put("/{user_id}",response_model=schemas.UserResponse)
 def update_user(user_id:int,user: schemas.UserUpdate,db:Session=Depends(get_db)):
     return crud.update_user(user_id,db,user_update=user)
+
 
 @router.delete("/{user_id}",status_code=status.HTTP_200_OK)
 def delete_user(user_id: int,db:Session=Depends(get_db)):
