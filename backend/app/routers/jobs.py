0ead60621fa8d4eb0a6d1ecae5ccfa3ad01d1c00
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-20 13:20:43 +0200

    udpated job endpoint

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -25,3 +25,10 @@ def get_job_by_id(job_id:int ,db:Session=Depends(get_db)):
 @router.get("/",response_model=list[schemas.JobResponse],status_code=status.HTTP_201_CREATED)
 def get_all_jobs(db:Session=Depends(get_db)):
     return crud.get_all_jobs(db)
+
+@router.put("/{job_id}",response_model=schemas.JobResponse)
+def update_job(job_id :int,job:schemas.JobUpdate,db:Session=Depends(get_db)):
+    updated_job=crud.update_job_by_id(db=db,job_id=job_id,updated_job=job)
+    if not updated_job:
+        raise HTTPException(status_code=404,detail="Job not found")
+    return updated_job
