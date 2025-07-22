1d3d5a2c7b5f28e507fd3e71376cae0301966ef4
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-22 13:31:20 +0200

    login with jwt endpoint

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,21 +1,17 @@
-from fastapi import Depends,HTTPException,status
-from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm  import Session
-from database import get_db
-from jwt_handler import decode_access_token
-import models
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+from app import models, utils, jwt_handler
+from app.schemas import schemas
+from app.database import get_db
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-def get_current_user(token: str=Depends(oauth2_scheme),db:Session=Depends(get_db)):
-    credentials_exception=HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid authentication credentials",
-        headers={"WWW=Authenticate":"Bearer"}
-    )
-    payload= decode_access_token(token)
-    if payload is None or "user_id" not in payload:
-        raise credentials_exception
-    user=db.query(models.User).filter(models.User.id==payload["user_id"]).first()
-    if user is None:
-        raise credentials_exception
-    return user
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+    if not user or not utils.verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
+
+    token = jwt_handler.create_access_token(data={"user_id": user.id})
+    return {"access_token": token, "token_type": "bearer"}
