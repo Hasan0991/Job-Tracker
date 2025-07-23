be83b095ae67185301b049d5550c24be18f6f3e9
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-23 15:51:23 +0200

    get companies endpoint

--- backend/app/routers/companies.py
+++ backend/app/routers/companies.py
@@ -1,4 +1,4 @@
-from fastapi import Depends,APIRouter,HTTPException,status
+from fastapi import Depends,APIRouter,HTTPException,status,Query
 from app.database import get_db
 from app.schemas import schemas
 from app import crud
@@ -9,3 +9,11 @@ router=APIRouter(prefix="/companies",tags=["Companies"])
 @router.post("/",response_model=schemas.CompanyResponse)
 def create_company(company:schemas.CompanyCreate,db:Session=Depends(get_db)):
     return crud.create_company(company=company,db=db)
+
+@router.get("/",response_model=list[schemas.CompanyResponse])
+def get_all_companies(
+    db:Session=Depends(get_db),
+    skip: int = Query(0, ge=0),
+    limit: int = Query(10, le=100)
+    ):
+    return crud.get_all_companies(db=db,skip=skip,limit=limit)
