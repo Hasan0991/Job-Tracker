ff2c6ac960d8a8ac363abd71747a93053db29e17
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-24 11:11:34 +0200

    fixed errors

--- backend/app/routers/jobs.py
+++ backend/app/routers/jobs.py
@@ -32,8 +32,8 @@ def get_all_jobs(
     return crud.get_jobs_paginated(db,skip=skip,limit=limit)
 
 @router.put("/{job_id}",response_model=schemas.JobResponse)
-def update_job(job_id :int,job:schemas.JobUpdate,db:Session=Depends(get_db)):
-    updated_job=crud.update_job_by_id(db=db,job_id=job_id,updated_job=job)
+def update_job(job_id :int,job:schemas.JobUpdate,current_user: User = Depends(get_current_user),db:Session=Depends(get_db)):
+    updated_job=crud.update_job_by_id(db=db,job_id=job_id,updated_job=job,current_user_id=current_user.id)
     if not updated_job:
         raise HTTPException(status_code=404,detail="Job not found")
     return updated_job
