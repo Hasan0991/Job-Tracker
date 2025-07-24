49f3a9def411fed9867fe861d024393f5ce4433a
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-24 20:29:48 +0200

    added role field

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -12,6 +12,7 @@ def create_user(db: Session,user:schemas.UserCreate):
     new_user= models.User(
         email=user.email,
         password=hashed_password,
+        role = user.role,
         first_name = user.first_name,
         last_name = user.last_name,
         birth_date = user.birth_date,
