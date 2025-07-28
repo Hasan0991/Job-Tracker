e7e6425759d28ecdcf677a690c368dc9753bbd3e
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-28 20:07:06 +0200

    tests for creating user and validation of credentials

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -6,3 +6,14 @@ from app.main import app
 def client():
     with TestClient(app) as c:
         yield c
+@pytest.fixture
+def admin_token_headers(client):
+    
+    login_data = {
+        "username": "admin@example.com",
+        "password": "adminpass"
+    }
+    response = client.post("/auth/login", data=login_data)
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+    return {"Authorization": f"Bearer {token}"}
--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -55,7 +55,7 @@ def test_admin_can_create_user(client):
     assert response.status_code == 201
     assert response.json()["email"] == payload["email"]
 
-def test_admin_can_create_user(client):
+def test_admin_can_create_same_user(client):
     login_data={
         "username": "admin@example.com",
         "password": "adminpass"
@@ -73,3 +73,61 @@ def test_admin_can_create_user(client):
     response = client.post("/users/", json=payload, headers=headers)
     assert response.status_code ==  400 
     assert response.json()["detail"] =="Email already exists"
+
+def test_create_user_empty_password(client, admin_token_headers):
+    payload = {
+        "email": "empty@example.com",
+        "password": "",  
+        "role": "user"
+    }
+    response = client.post("/users/", json=payload, headers=admin_token_headers)
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["msg"] == "String should have at least 8 characters"
+
+
+def test_user_auth_wrong_email(client):
+    login_data={
+        "username": "admin@example.com",
+        "password": "adminpass"
+    }
+    login_response=client.post("/auth/login",data=login_data)
+    assert login_response.status_code==200
+    token = login_response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {token}"}
+
+    payload = {
+        "email": "testuser2@example",# without .com
+        "password": "testpass123", 
+        "role": "user"
+    }
+    response = client.post("/users/", json=payload, headers=headers)
+    assert response.status_code ==  422 
+    assert response.json()["detail"][0]["msg"] =="value is not a valid email address: The part after the @-sign is not valid. It should have a period."
+
+def test_user_post_user(client):
+    login_data={
+        "username":"testuser@example.com",
+        "password":"testpassword"
+    }
+    login_response=client.post("/auth/login",data=login_data)
+    assert login_response.status_code==200
+    token = login_response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {token}"}
+    payload = {
+        "email": "testuser3@example.com",
+        "password": "testpass123", 
+        "role": "user"
+    }
+    response = client.post("/users/", json=payload, headers=headers)
+    assert response.status_code ==  403
+    assert response.json()["detail"]=="Not authorized to create this user"
+
+def test_create_user_short_password(client,admin_token_headers):
+    payload = {
+            "email": "testuser4@example.com",
+            "password": "123", 
+            "role": "user"
+        }
+    response=client.post("/auth/register",json=payload,headers=admin_token_headers)
+    assert response.status_code==422
+    assert "String should have at least 8 characters" in response.json()["detail"][0]["msg"]
