a6eb4b168d5db7ee41222268eb848fde36ac3b3d
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-30 11:52:54 +0200

    changed the order for swagger

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,11 +12,10 @@ app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(jobs.router)
-app.include_router(auth.router)
-app.include_router(protected.router)
 app.include_router(companies.router)
 app.include_router(applications.router)
-
+app.include_router(auth.router)
+app.include_router(protected.router)
 @app.get("/")
 def root():
     return {"message": "Job Tracker backend is running"}
