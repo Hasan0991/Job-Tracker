193c253718c68189a937649b27e225610f427bf3
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-30 11:47:18 +0200

    fixed issues in testing

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -12,8 +12,8 @@ def test_register_user(client):
 def test_login_user(client):
     response=client.post("/auth/login",
         data={
-            "username":"testuser@example.com",
-            "password":"testpassword"
+            "username":"testuser1@example.com",
+            "password":"testpassword1"
         })
     assert response.status_code==200
     assert "access_token" in response.json()
@@ -35,42 +35,24 @@ def test_incorrect_register_user(client):
     response=client.post("/auth/register",json=payload)
     assert response.status_code==422
 
-def test_admin_can_create_user(client):
-  
-    login_data = {
-        "username": "admin@example.com",
-        "password": "adminpass"
-    }
-    login_response = client.post("/auth/login", data=login_data)
-    assert login_response.status_code == 200
-    token = login_response.json()["access_token"]
-    headers = {"Authorization": f"Bearer {token}"}
+def test_admin_can_create_user(client,admin_token_headers):
 
     payload = {
         "email": "testuser2@example.com",
         "password": "testpass123",
         "role": "user"
     }
-    response = client.post("/users/", json=payload, headers=headers)
+    response = client.post("/users/", json=payload, headers=admin_token_headers)
     assert response.status_code == 201
     assert response.json()["email"] == payload["email"]
 
-def test_admin_can_create_same_user(client):
-    login_data={
-        "username": "admin@example.com",
-        "password": "adminpass"
-    }
-    login_response=client.post("/auth/login",data=login_data)
-    assert login_response.status_code==200
-    token = login_response.json()["access_token"]
-    headers = {"Authorization": f"Bearer {token}"}
-
+def test_admin_can_create_same_user(client,admin_token_headers):
     payload = {
-        "email": "testuser2@example.com",
+        "email": "testuser1@example.com",
         "password": "testpass123",
         "role": "user"
     }
-    response = client.post("/users/", json=payload, headers=headers)
+    response = client.post("/users/", json=payload, headers=admin_token_headers)
     assert response.status_code ==  400 
     assert response.json()["detail"] =="Email already exists"
 
@@ -85,29 +67,22 @@ def test_create_user_empty_password(client, admin_token_headers):
     assert response.json()["detail"][0]["msg"] == "String should have at least 8 characters"
 
 
-def test_user_auth_wrong_email(client):
-    login_data={
-        "username": "admin@example.com",
-        "password": "adminpass"
-    }
-    login_response=client.post("/auth/login",data=login_data)
-    assert login_response.status_code==200
-    token = login_response.json()["access_token"]
-    headers = {"Authorization": f"Bearer {token}"}
+def test_user_auth_wrong_email(client,admin_token_headers):
+  
 
     payload = {
         "email": "testuser2@example",# without .com
         "password": "testpass123", 
         "role": "user"
     }
-    response = client.post("/users/", json=payload, headers=headers)
+    response = client.post("/users/", json=payload, headers=admin_token_headers)
     assert response.status_code ==  422 
     assert response.json()["detail"][0]["msg"] =="value is not a valid email address: The part after the @-sign is not valid. It should have a period."
 
 def test_user_post_user(client):
     login_data={
-        "username":"testuser@example.com",
-        "password":"testpassword"
+        "username":"testuser1@example.com",
+        "password":"testpassword1"
     }
     login_response=client.post("/auth/login",data=login_data)
     assert login_response.status_code==200
