a74827eba6b969c5407e01e22e151f329983dcaf
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-07-30 22:51:29 +0200

    all user tests

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -98,3 +98,63 @@ def test_create_user_short_password(client,admin_token_headers):
     response=client.post("/auth/register",json=payload,headers=admin_token_headers)
     assert response.status_code==422
     assert "String should have at least 8 characters" in response.json()["detail"][0]["msg"]
+
+def test_admin_get_users(client,admin_token_headers):
+
+    response = client.get("/users/",headers=admin_token_headers)
+    assert response.status_code==200
+    assert isinstance(response.json(),list)
+
+def test_admin_get_user_by_id(client,admin_token_headers):
+    response=client.get("/users/1",headers=admin_token_headers)
+    assert response.status_code==200
+    assert response.json()["email"]=="testuser1@example.com"
+
+def test_user_get_user_by_id(client,user_token_headers):
+    response=client.get("/users/2",headers=user_token_headers)
+    assert response.status_code==403
+    assert response.json()["detail"]=="Not authorized to view this user"
+
+def test_user_get_own_info(client,user_token_headers):
+    response=client.get("/users/users/me",headers=user_token_headers)
+    assert response.status_code==200
+    assert response.json()["email"]=="testuser1@example.com"
+
+def test_user_get_own_info_without_auth(client,user_token_headers):
+    response=client.get("/users/users/me")
+    assert response.status_code==401
+    assert response.json()["detail"]=="Not authenticated"
+
+def test_admin_can_update_user(client, admin_token_headers):
+    payload = {
+        "first_name": "Updated",
+        "last_name": "User"
+    }
+    response = client.put("/users/1", json=payload, headers=admin_token_headers)
+    assert response.status_code == 200
+    assert response.json()["first_name"] == "Updated"
+
+def test_user_cannot_update_user(client, user_token_headers):
+    payload = {
+        "first_name": "Not Updated",
+        "last_name": "User"
+    }
+    response = client.put("/users/2", json=payload, headers=user_token_headers)
+    assert response.status_code == 403
+    assert response.json()["detail"] == "Not authorized to update this user"
+
+def test_admin_can_delete_user(client,admin_token_headers):
+    response = client.delete("/users/1", headers=admin_token_headers)
+    assert response.status_code == 200
+    assert response.json()["details"] == "user deleted"
+
+def test_user_cannot_delete_user(client,user_token_headers):
+    response = client.delete("/users/2", headers=user_token_headers)
+    assert response.status_code == 403
+    assert response.json()["detail"] == "Not authorized to delete this user"
+
+
+def test_user_can_delete_himself(client,user_token_headers):
+    response = client.delete("/users/1", headers=user_token_headers)
+    assert response.status_code == 200
+    assert response.json()["details"] == "user deleted"
