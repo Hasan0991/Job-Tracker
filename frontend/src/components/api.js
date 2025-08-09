41aec913bc8360932557713927e2026b06179b8a
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-08-09 20:00:51 +0200

    replaced with reailway url

---
+++ frontend/src/components/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "https://ravishing-vibrancy-production.up.railway.app", 
+});
+
+export default API;
