59173b97126312518fda178d8cd7a3c4ecfb2a46
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-08-09 14:47:23 +0200

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routers import users, jobs, auth, protected, companies, applications
 from app import models
 from app.database import engine, Base,init_db 
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="Job Tracker API",
     description="API for managing users, jobs, companies, and applications.",
@@ -15,7 +15,19 @@ app = FastAPI(
         "name": "MIT License",
     }
 )
+origins = [
+    "http://localhost:3000",  # адрес твоего React (можешь добавить ещё если нужно)
+    "https://your-frontend-domain.com",
+    "*",  # если хочешь разрешить всем (для теста, но не рекомендовано на проде)
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # разрешённые источники
+    allow_credentials=True,
+    allow_methods=["*"],  # разрешить все методы (POST, GET, PUT...)
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
@@ -31,3 +43,7 @@ app.include_router(protected.router)
 @app.get("/")
 def root():
     return {"message": "Job Tracker backend is running"}
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
